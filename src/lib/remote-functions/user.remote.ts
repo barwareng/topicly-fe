af950e7ba2ae6aae0652ab1415b53bd7488d1d34
barwareng <cleophas.barwareng@gmail.com>
2025-11-17 05:13:17 +0300

    wip:auth

--- src/lib/remote-functions/user.remote.ts
+++ src/lib/remote-functions/user.remote.ts
@@ -1,34 +1,19 @@
 import { form, getRequestEvent } from '$app/server';
 import { usersSchema } from '$lib/schemas/user.schema';
 import { redirect } from '@sveltejs/kit';
-import { ClientResponseError } from 'pocketbase';
 // import { Collections } from '$lib/constants/collections';
 
 export const sendOtp = form(usersSchema.pick({ email: true }), async (data) => {
 	const { locals, cookies } = await getRequestEvent();
 	try {
-		await locals.pb.collection('users').getOne(`email="${data.email}"`);
-
-		const { otpId } = await locals.pb.collection('users').requestOTP(data.email);
+		const { otpId } = await locals.pb.send('/api/send-otp-email', {
+			method: 'POST',
+			body: JSON.stringify({ email: data.email })
+		});
 		console.log('OTP sent result:', otpId);
 		cookies.set('otpId', otpId, { path: '/' });
 		redirect(303, '/verify-otp');
 	} catch (error) {
-		if (error instanceof ClientResponseError) {
-			console.log('ClientResponseError:', error);
-			if (error.status === 404) {
-				// User not found, create a new user
-				console.log('User not found, creating new user');
-				const password = crypto.randomUUID();
-				await locals.pb.collection('users').create({
-					email: data.email,
-					password,
-					passwordConfirm: password
-				});
-				const { otpId } = await locals.pb.collection('users').requestOTP(data.email);
-				cookies.set('otpId', otpId, { path: '/' });
-				redirect(303, '/verify-otp');
-			}
-		}
+		return { success: false, message: 'Failed to send OTP. Please try again.' };
 	}
 });
