8eff46cbec8d636ccbc5b59f4d6a1630018c30ba
barwareng <cleophas.barwareng@gmail.com>
2025-11-17 05:53:15 +0300

    wip: dashboard

--- src/lib/remote-functions/user.remote.ts
+++ src/lib/remote-functions/user.remote.ts
@@ -1,6 +1,7 @@
 import { form, getRequestEvent } from '$app/server';
 import { usersSchema } from '$lib/schemas/user.schema';
 import { redirect } from '@sveltejs/kit';
+import { z } from 'zod/v4';
 // import { Collections } from '$lib/constants/collections';
 
 export const sendOtp = form(usersSchema.pick({ email: true }), async (data) => {
@@ -13,6 +14,18 @@ export const sendOtp = form(usersSchema.pick({ email: true }), async (data) => {
 		console.log('OTP sent result:', otpId);
 		cookies.set('otpId', otpId, { path: '/' });
 		redirect(303, '/verify-otp');
+	} catch (error) {
+		return { success: false, message: 'Failed to send OTP. Please try again.' };
+	}
+});
+export const verifyOtp = form(z.object({ otp: z.string() }), async (data) => {
+	const { locals, cookies } = await getRequestEvent();
+	try {
+		const otpId = cookies.get('otpId');
+		console.log('Verifying OTP with ID:', otpId, 'and code:', data.otp);
+		const authData = await locals.pb.collection('users').authWithOTP(otpId!, data.otp);
+		console.log('OTP verification result:', authData);
+		redirect(303, '/home');
 	} catch (error) {
 		return { success: false, message: 'Failed to send OTP. Please try again.' };
 	}
--- src/routes/(app)/start/+layout@.svelte
+++ src/routes/(app)/start/+layout@.svelte
@@ -1,15 +1,18 @@
 <script lang="ts">
-	import AppSidebar from "$lib/components/app-sidebar.svelte";
-	import NavActions from "$lib/components/nav-actions.svelte";
-	import * as Breadcrumb from "$lib/components/ui/breadcrumb/index.js";
-	import { Separator } from "$lib/components/ui/separator/index.js";
-	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+	import AppSidebar from '$lib/components/reusable/nav/sidebar/app-sidebar.svelte';
+	// import NavActions from '$lib/components/nav-actions.svelte';
+	import * as Breadcrumb from '$lib/components/ui/breadcrumb/index.js';
+	import { Separator } from '$lib/components/ui/separator/index.js';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import type { LayoutProps } from './$types';
+
+	let { data, children }: LayoutProps = $props();
 </script>
 
 <Sidebar.Provider>
 	<AppSidebar />
 	<Sidebar.Inset>
-		<header class="flex h-14 shrink-0 items-center gap-2">
+		<!-- <header class="flex h-14 shrink-0 items-center gap-2">
 			<div class="flex flex-1 items-center gap-2 px-3">
 				<Sidebar.Trigger />
 				<Separator orientation="vertical" class="mr-2 data-[orientation=vertical]:h-4" />
@@ -26,10 +29,9 @@
 			<div class="ml-auto px-3">
 				<NavActions />
 			</div>
-		</header>
+		</header> -->
 		<div class="flex flex-1 flex-col gap-4 px-4 py-10">
-			<div class="bg-muted/50 mx-auto h-24 w-full max-w-3xl rounded-xl"></div>
-			<div class="bg-muted/50 mx-auto h-full w-full max-w-3xl rounded-xl"></div>
+			{@render children()}
 		</div>
 	</Sidebar.Inset>
 </Sidebar.Provider>
